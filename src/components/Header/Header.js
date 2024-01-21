3fc4258631ef63342d1f7f0971e48cc90c59d565
ninhtrong2k <ninhtrongchannel@gmail.com>
2024-01-21 13:34:05 +0700

    video 51

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand>
+                <NavLink to="/" className='navbar-brand'>Hỏi Dân IT</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -20,14 +20,14 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item >Action</NavDropdown.Item>
+                            <NavDropdown.Item >
                                 Another action
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Login</NavDropdown.Item>
+                            <NavDropdown.Item >Login</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Log Out</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Log Out</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
 
                         </NavDropdown>
                     </Nav>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,8 @@
+const HomePage = (props) => {
+    return (
+        <div className="homepage-container">
+            Home page 
+        </div>
+    )
+}
+export default HomePage;
