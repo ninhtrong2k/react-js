f07945d96f6d0d54634f86dc2e37780e8a62e389
ninhtrong2k <ninhtrongchannel@gmail.com>
2024-01-25 13:21:54 +0700

    video 59

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,6 +3,9 @@ import './Admin.scss';
 import { FaBars } from 'react-icons/fa';
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Admin = (props) => {
     const [collapsed, setCollapsed] = useState(false);
@@ -14,12 +17,23 @@ const Admin = (props) => {
             </div>
             <div className="admin-content">
                 <div className="admin-header">
-                <FaBars onClick={() => setCollapsed(!collapsed)} />
+                    <FaBars onClick={() => setCollapsed(!collapsed)} />
                 </div>
                 <div className="admin-main">
-                <Outlet/>
+                    <Outlet />
                 </div>
             </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                />
         </div>
     )
 }
--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -4,15 +4,19 @@ import Modal from 'react-bootstrap/Modal';
 import './ManageUser.scss';
 import { FcPlus } from "react-icons/fc";
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
     // const [show, setShow] = useState(false);
 
-    const handleClose = () => { 
-        setShow(false) 
-        setEmail ("");
+    const handleClose = () => {
+        setShow(false)
+        setEmail("");
         setPassword("");
         setUserName("");
         setRole("ADMIN");
@@ -38,6 +42,14 @@ const ModalCreateUser = (props) => {
         // console.log("upload files", event.target.files[0]);
     }
 
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
     const handleSubmitCreateUser = async () => {
         // let data = {
         //     email: email,
@@ -47,7 +59,15 @@ const ModalCreateUser = (props) => {
         //     userImage: image,
         // }
         // console.log(data)
-        const FormData = require('form-data');
+        const isValidateEmail = validateEmail(email);
+        if (!isValidateEmail){
+            toast.error("Invalid email");
+            return;
+        
+        }
+
+
+            const FormData = require('form-data');
         const data = new FormData();
         data.append('email', email);
         data.append('password', password);
@@ -55,7 +75,14 @@ const ModalCreateUser = (props) => {
         data.append('role', role);
         data.append('userImagem', image);
         let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-        console.log(res);
+        if(res.data && res.data.EC === 0){
+            toast.success(res.data.EM)
+            handleClose();
+        };
+        if(res.data && res.data.EC !== 0){
+            toast.error(res.data.EM)
+        }
+
     }
 
     return (
@@ -138,6 +165,7 @@ const ModalCreateUser = (props) => {
                     </Button>
                 </Modal.Footer>
             </Modal>
+            
         </>
     );
 }
