2231b25b54f607b215f6c6246fd7dbb3a05d6d22
ninhtrong2k <ninhtrongchannel@gmail.com>
2024-01-30 16:17:37 +0700

    video 84

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink, useNavigate } from "react-router-dom";  /// điều hướng
 import { useSelector } from 'react-redux';
-
 const Header = () => {
     const isAuthenticated = useSelector(state => state.user.isAuthenticated)
     const account = useSelector(state => state.user.account)
--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams , useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
 import _ from "lodash";
+import './DetailQuiz.scss';
 const DetailQuiz = (props) => {
     const params = useParams();
+    const location = useLocation();
     const quizId = params.id;
     useEffect(() => {
         fetchQuestions();
@@ -40,7 +42,31 @@ const DetailQuiz = (props) => {
     console.log('check params :', params)
     return (
         <div className="detail-quiz-container">
-            DetailQuiz
+           <div className="left-content">
+            <div className="title">
+               Quiz {quizId} :  {location?.state?.quizTitle}
+               <hr></hr>
+            </div>
+            <div className="q-body">
+                <img></img>
+            </div>
+            <div className="q-content">
+                <div className="question">Question 1: How are you doing ?</div>
+                <div className="answer">
+                    <div className="a-child">A. Sksadksak</div>
+                    <div className="a-child">B. Sksadksak</div>
+                    <div className="a-child">C. Sksadksak</div>
+                </div>
+            </div>
+            <div className="footer">
+                <button className="btn btn-secondary ">Prev</button>
+                <button className="btn btn-primary">Next</button>
+            </div>
+           </div>
+           <div className="right-content">
+                count down
+
+           </div>
         </div>
     )
 }
