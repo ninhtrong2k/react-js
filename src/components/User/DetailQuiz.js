f940d5ad12462db3677433489622eb925133bd80
ninhtrong2k <ninhtrongchannel@gmail.com>
2024-01-30 20:38:51 +0700

    video 85

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
-import { useParams , useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
 import _ from "lodash";
 import './DetailQuiz.scss';
+import Question from "./Question";
 const DetailQuiz = (props) => {
     const params = useParams();
     const location = useLocation();
     const quizId = params.id;
+
+    const [dataQuiz, setDataQuiz] = useState("");
+    const [index, setIndex] = useState(0);
     useEffect(() => {
         fetchQuestions();
     }, [quizId])
@@ -20,53 +24,66 @@ const DetailQuiz = (props) => {
                 .groupBy("id")
                 // `key` is group's name (color), `value` is the array of objects
                 .map((value, key) => {
-                    let answers  = [];
-                    let questionDescription, image = null ;
+                    let answers = [];
+                    let questionDescription, image = null;
 
                     value.forEach((item, index) => {
-                        if(index === 0){
+                        if (index === 0) {
                             questionDescription = item.description
                             image = item.image;
                         }
                         answers.push(item.answers);
-                        console.log('item',item.answers)
+                        console.log('item', item.answers)
                     })
-                    console.log('value',value , 'key',key)
-                    return { questionId: key, answers ,questionDescription , image}
+                    console.log('value', value, 'key', key)
+                    return { questionId: key, answers, questionDescription, image }
                 }
                 )
                 .value()
             console.log(data)
+            setDataQuiz(data);
         }
     }
-    console.log('check params :', params)
+    console.log('check data quiz :', dataQuiz)
+    const handlePrev = () => {
+        if(index -1 < 0) return;
+        setIndex(index - 1)
+    }
+    const handleNext = () => {
+        if(dataQuiz && dataQuiz.length > index + 1)
+        setIndex(index + 1)
+    }
     return (
         <div className="detail-quiz-container">
-           <div className="left-content">
-            <div className="title">
-               Quiz {quizId} :  {location?.state?.quizTitle}
-               <hr></hr>
-            </div>
-            <div className="q-body">
-                <img></img>
-            </div>
-            <div className="q-content">
-                <div className="question">Question 1: How are you doing ?</div>
-                <div className="answer">
-                    <div className="a-child">A. Sksadksak</div>
-                    <div className="a-child">B. Sksadksak</div>
-                    <div className="a-child">C. Sksadksak</div>
+            <div className="left-content">
+                <div className="title">
+                    Quiz {quizId} :  {location?.state?.quizTitle}
+                    <hr></hr>
+                </div>
+                <div className="q-body">
+                    <img></img>
+                </div>
+                <div className="q-content">
+                    <Question
+                        index={index}
+                        data={dataQuiz && dataQuiz.length > 0 ? dataQuiz[index] : []}
+                    />
+                </div>
+                <div className="footer">
+                    <button 
+                    className="btn btn-secondary"
+                    onClick={() => handlePrev()}
+                    >Prev</button>
+                    <button 
+                    className="btn btn-primary"
+                    onClick={() => handleNext()}
+                    >Next</button>
                 </div>
             </div>
-            <div className="footer">
-                <button className="btn btn-secondary ">Prev</button>
-                <button className="btn btn-primary">Next</button>
-            </div>
-           </div>
-           <div className="right-content">
+            <div className="right-content">
                 count down
 
-           </div>
+            </div>
         </div>
     )
 }
